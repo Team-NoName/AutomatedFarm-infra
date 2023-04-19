24c669b58f2182fa75b4178de2c67e969221a19c
Saalim <danascape@gmail.com>
2023-04-19 18:21:47 +0530

    src: Check for package before installation
    Signed-off-by: Saalim <danascape@gmail.com>

--- src/read-uart.sh
+++ src/read-uart.sh
@@ -6,4 +6,8 @@
 # SPDX-License-Identifier: Apache-2.0 license
 #
 
+if [[ -z "$(which picocom)" ]]; then
+    echo "error: Install picocom package!"
+fi
+
 sudo picocom -b 9600 /dev/ttyACM1 | tee /home/saalim/AutomatedFarm-infra/logs.txt
